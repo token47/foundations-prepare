84a81e0f1b2a6881edb19a4c0d977dff084a17bf
Andre <andre.ruiz@canonical.com>
2019-03-28 19:21:00 +0000

    changes

--- post2.sh
+++ post2.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-echo "$(ssh 192.168.210.4 "sudo -u maas ssh-keygen -y -f /var/lib/maas/.ssh/id_rsa") maas@infra1" >> .ssh/authorized_keys
+key="$(ssh 192.168.210.4 "sudo -u maas ssh-keygen -y -f /var/lib/maas/.ssh/id_rsa")"
+
+if [ $? -eq 0 ]; then
+	echo "$key maas@infra1" >> ~/.ssh/authorized_keys
+	echo "Done"
+else
+	echo "No key found (yet)"
+fi
 
